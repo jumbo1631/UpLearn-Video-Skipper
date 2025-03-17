ed5720aedd656ce17b6e3145148ec260f5aa3515
jumbo1631 <PastimeEpilogue1Manila@proton.me>
2025-03-17 19:24:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,106 @@
-const button = document.createElement("button");
-let video = document.querySelector("video");
-button.innerText = "Skip Video";
-button.style.position = "absolute";
-button.style.left = "10px";
-button.style.top = "100px";
-button.style.backgroundColor = "red";
-button.style.color = "white";
-button.style.border = "none";
-button.style.padding = "10px";
-button.style.cursor = "pointer";
+// ==UserScript==
+// @name         Up Learn Video Skip
+// @namespace    http://tampermonkey.net/
+// @version      2025-03-12
+// @description  Skip videos efficiently on Up Learn, including those that contain intermittent questions.
+// @author       jumbo1631
+// @match        https://web.uplearn.co.uk/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// ==/UserScript==
 
-document.body.appendChild(button);
 
-let questionTimes = [];
-let nextTimeIndex = 0;
-let finished = false;
+// ******************************************************************************
 
-function processTime(postQuestionResumeTime) {
-    const timeString = postQuestionResumeTime.replace("PT", "");
-    const timeParts = timeString.match(/(\d+)M(\d+\.\d+)S/);
-    if (timeParts) {
-        const minutes = parseInt(timeParts[1], 10);
-        const seconds = parseFloat(timeParts[2]);
-        const totalSeconds = Math.floor(minutes * 60 + seconds) - 1;
-        questionTimes.push(totalSeconds);
-        console.log(totalSeconds);
-    } else {
-        const seconds = parseFloat(timeString.replace("S", ""));
-        const totalSeconds = Math.floor(seconds) - 1;
-        questionTimes.push(totalSeconds);
-        console.log(totalSeconds);
+// This token should be of the form "Bearer some_long_alphanumeric_key"
+// Refer to the documentation for where to get this.
+
+const API_BEARER_TOKEN = "";
+
+// ******************************************************************************
+
+(function() {
+    'use strict';
+
+    const button = document.createElement("button");
+    let video = document.querySelector("video");
+    button.innerText = "Skip Video";
+    button.style.position = "absolute";
+    button.style.left = "10px";
+    button.style.top = "100px";
+    button.style.backgroundColor = "red";
+    button.style.color = "white";
+    button.style.border = "none";
+    button.style.padding = "10px";
+    button.style.cursor = "pointer";
+
+    document.body.appendChild(button);
+
+    let questionTimes = [];
+    let nextTimeIndex = 0;
+    let finished = false;
+
+    function processTime(postQuestionResumeTime) {
+        const timeString = postQuestionResumeTime.replace("PT", "");
+        const timeParts = timeString.match(/(\d+)M(\d+\.\d+)S/);
+        if (timeParts) {
+            const minutes = parseInt(timeParts[1], 10);
+            const seconds = parseFloat(timeParts[2]);
+            const totalSeconds = Math.floor(minutes * 60 + seconds) - 1;
+            questionTimes.push(totalSeconds);
+            console.log(totalSeconds);
+        } else {
+            const seconds = parseFloat(timeString.replace("S", ""));
+            const totalSeconds = Math.floor(seconds) - 1;
+            questionTimes.push(totalSeconds);
+            console.log(totalSeconds);
+        }
     }
-}
 
-const makeApiRequest = () => {
-    video = document.querySelector("video")
-    const url = window.location.href;
-    const urlParts = url.split('/');
-    const moduleUniqueCode = urlParts[4];
-    const subsectionUniqueCode = urlParts[5];
-    const uniqueCode = urlParts[6];
+    const makeApiRequest = () => {
 
-    fetch("https://web.uplearn.co.uk/api/", {
-        headers: {
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "content-type": "application/json",
-            "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin"
-        },
-        credentials: "include",
-        body: JSON.stringify({
-            operationName: "GetVideoLesson",
-            variables: {
-                moduleUniqueCode,
-                subsectionUniqueCode,
-                uniqueCode,
-                inVideoQuestionOrder: [{
-                    field: "TRIGGER_TIME",
-                    direction: "ASC"
-                }]
+        questionTimes = [];
+        nextTimeIndex = 0;
+        finished = false;
+
+        video = document.querySelector("video")
+
+        if(!video) {
+            alert('No video found on the current page');
+            return;
+        }
+
+        const url = window.location.href;
+        const urlParts = url.split('/');
+        const moduleUniqueCode = urlParts[4];
+        const subsectionUniqueCode = urlParts[5];
+        const uniqueCode = urlParts[6];
+
+        fetch("https://web.uplearn.co.uk/api/", {
+            headers: {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "content-type": "application/json",
+                "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "authorization": API_BEARER_TOKEN
             },
-            query: `
+            credentials: "include",
+            body: JSON.stringify({
+                operationName: "GetVideoLesson",
+                variables: {
+                    moduleUniqueCode,
+                    subsectionUniqueCode,
+                    uniqueCode,
+                    inVideoQuestionOrder: [{
+                        field: "TRIGGER_TIME",
+                        direction: "ASC"
+                    }]
+                },
+                query: `
             query GetVideoLesson(
                 $uniqueCode: String!,
                 $subsectionUniqueCode: String!,
@@ -88,50 +123,69 @@ const makeApiRequest = () => {
                 }
             }
             `
-        }),
-        method: "POST"
-    })
-    .then(response => response.json())
-    .then(data => {
-        const info = data.data.videoLesson.inVideoQuizQuestions;
+            }),
+            method: "POST"
+        })
+            .then(response => {
+            const statusCode = response.status;
+            if(statusCode == 401 ) {
+                alert('There was a problem authenticating. You may need to set - or refresh - the bearer token above. Refer to the documentation for details.');
+                return;
+            }
+            else if(statusCode != 200) {
+                alert('An unknown error occurred.');
+                return;
+            }
+            return response.json();
+        })
+            .then(data => {
 
-        for (let question of info) {
-            processTime(question.postQuestionResumeTime);
-        }
+            if(data.errors && data.errors[0].message == 'unauthenticated'){
+                alert('There was a problem authenticating. You may need to set - or refresh - the bearer token above. Refer to the documentation for details.');
+                return;
+            }
 
-        if (questionTimes.length > 0) {
-            video.currentTime = questionTimes[nextTimeIndex];
-        }
+            const info = data.data.videoLesson.inVideoQuizQuestions;
 
-        video.addEventListener('timeupdate', () => {
-            if (finished) { return; }
-            const currentTime = video.currentTime;
-            console.log("Current Time:", currentTime, "Next Time:", questionTimes[nextTimeIndex]);
+            for (let question of info) {
+                processTime(question.postQuestionResumeTime);
+            }
 
-            if (nextTimeIndex < questionTimes.length) {
-                if (currentTime - 2 >= questionTimes[nextTimeIndex]) {
-                    console.log(`Skipping to: ${questionTimes[nextTimeIndex]} seconds`);
-                    nextTimeIndex++;
+            if (questionTimes.length > 0) {
+                video.currentTime = questionTimes[nextTimeIndex];
+            }
 
-                    if (nextTimeIndex < questionTimes.length) {
-                        video.currentTime = questionTimes[nextTimeIndex] - 1;
-                    } else {
-                        console.log("No more questions. Skipping to the end of the video.");
-                        video.currentTime = video.duration - 0.5;
+            video.addEventListener('timeupdate', () => {
+                if (finished) { return; }
+                const currentTime = video.currentTime;
+                console.log("Current Time:", currentTime, "Next Time:", questionTimes[nextTimeIndex]);
+
+                if (nextTimeIndex < questionTimes.length) {
+                    if (currentTime - 2 >= questionTimes[nextTimeIndex]) {
+                        console.log(`Skipping to: ${questionTimes[nextTimeIndex]} seconds`);
+                        nextTimeIndex++;
+
+                        if (nextTimeIndex < questionTimes.length) {
+                            video.currentTime = questionTimes[nextTimeIndex] - 1;
+                        } else {
+                            console.log("No more questions. Skipping to the end of the video.");
+                            video.currentTime = video.duration - 0.5;
+                        }
                     }
+                } else {
+                    console.log("Next time index is out of bounds. Skipping to the end of the video.");
+                    video.currentTime = video.duration - 0.5;
                 }
-            } else {
-                console.log("Next time index is out of bounds. Skipping to the end of the video.");
-                video.currentTime = video.duration - 0.5;
-            }
 
-            if (currentTime >= video.duration - 0.5) {
-                finished = true;
-                console.log("Reached near the end. Stopping playback.");
-            }
-        });
-    })
-    .catch(error => console.error("Error:", error));
-};
+                if (currentTime >= video.duration - 0.5) {
+                    finished = true;
+                    console.log("Reached near the end. Stopping playback.");
+                }
+            });
+        })
+            .catch(error => console.error("Error:", error));
+    };
 
-button.addEventListener("click", makeApiRequest);
+    button.addEventListener("click", makeApiRequest);
+
+})();
